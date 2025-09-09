51213bd27ccb993c610f7e56c17d036b5249b896
alexlimon404 <mirrorbitmirror@gmail.com>
2025-09-09 14:34:36 +0300

    fix

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo -u www-data git pull
+
+sudo -u www-data composer install --no-dev --no-interaction --optimize-autoloader --prefer-dist
+
+php artisan migrate --force
+
+php artisan install
+
+php artisan config:cache
+
+php artisan event:cache
+
+php artisan view:clear
+
+php artisan filament:optimize
+
+#php artisan horizon:terminate
