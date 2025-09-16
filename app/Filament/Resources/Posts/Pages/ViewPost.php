4f8f8c83aad3c2e263e4a9f540473b4ec1c2c2bc
alexlimon404 <mirrorbitmirror@gmail.com>
2025-09-16 23:18:34 +0300

    fix

--- app/Filament/Resources/Posts/Pages/ViewPost.php
+++ app/Filament/Resources/Posts/Pages/ViewPost.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Actions\EditAction;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Notifications\Notification;
 
 class ViewPost extends ViewRecord
 {
@@ -13,6 +15,25 @@ class ViewPost extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('publish')
+                ->label($this->record->isPublished() ? 'Unpublish' : 'Publish')
+                ->icon($this->record->isPublished() ? 'heroicon-o-eye-slash' : 'heroicon-o-eye')
+                ->color($this->record->isPublished() ? 'danger' : 'success')
+                ->action(function () {
+                    if ($this->record->isPublished()) {
+                        $this->record->update(['published_at' => null]);
+                        Notification::make()
+                            ->title('Post unpublished successfully')
+                            ->danger()
+                            ->send();
+                    } else {
+                        $this->record->update(['published_at' => now()]);
+                        Notification::make()
+                            ->title('Post published successfully')
+                            ->success()
+                            ->send();
+                    }
+                }),
             EditAction::make(),
         ];
     }
